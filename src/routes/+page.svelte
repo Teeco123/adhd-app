b7270a209a348306e36969aa9687abd74af46acf
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-15 17:11:33 +0200

    adding new subtasks with different names

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,32 @@
 
 	const tasks: Task[] = $state([
 		{ id: 1, name: 'gowno', subtasks: [{ id: 121, name: 'dupaaa', status: false }] },
-		{ id: 2, name: 'gownoda', subtasks: [{ id: 121, name: 'dupa', status: false }] },
-		{ id: 3, name: 'gownodadadadadadadada' }
+		{ id: 2, name: 'gownoda', subtasks: [{ id: 124, name: 'dupa', status: false }] },
+		{ id: 3, name: 'gownodadadadadadadada', subtasks: [] }
 	]);
 
 	let taskName = $state('New task');
+	let subtaskInputs = $state<{ [key: number]: string }>({});
 
 	function addTask() {
-		tasks.push({ id: 4, name: taskName });
+		tasks.push({ id: Date.now(), name: taskName });
 		taskName = '';
+	}
+
+	function addSubtask(taskId: number) {
+		const name = subtaskInputs[taskId];
+		if (!name) return;
+
+		const foundTask = tasks.find((task) => task.id === taskId);
+		if (foundTask) {
+			if (!foundTask.subtasks) foundTask.subtasks = [];
+			foundTask.subtasks.push({
+				id: Date.now(), // unique id
+				name,
+				status: false
+			});
+			subtaskInputs[taskId] = ''; // clear input
+		}
 	}
 </script>
 
@@ -40,11 +57,12 @@
 								<div class="subtask-text">{subtask.name}</div>
 							</div>
 						{/each}
-						<div class="new-task">
-							<input type="text" />
-						</div>
 					</div>
 				{/if}
+				<div class="new-task">
+					<input type="text" bind:value={subtaskInputs[task.id]} />
+					<button onclick={() => addSubtask(task.id)}>-></button>
+				</div>
 			</div>
 		{/each}
 		<div class="new-task">
